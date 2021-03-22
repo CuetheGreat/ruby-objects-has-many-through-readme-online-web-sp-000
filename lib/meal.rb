8409c74dfa251482ebc6c261f29bbbeb70369cf2
Clayon Stewart <c.stewart89@gmail.com>
2021-03-22 21:49:38 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
-
+  
+  @@all = []
+  
+  def initialize(meal, customer, total, tip)
+    @meal = meal
+    @customer = customer
+    @total = total
+    @tip = tip
+    save
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+  def meals 
+    @customer.all
 end
